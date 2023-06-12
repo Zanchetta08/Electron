532c4e0be6ffbd9bc34e84edcc887e22c56b2c76
Zanchetta08 <110983552+Zanchetta08@users.noreply.github.com>
2023-06-12 18:56:54 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { app, BrowserWindow } = require("electron");
 
 function createWindow(){
-    const win = new BrowserWindow({ width: 800, height: 600 });
+    const win = new BrowserWindow({ 
+        width: 800,
+        height: 600,
+        titleBarStyle:'hidden',
+        fullscreen:'true'
+     });
     win.loadURL("http://localhost:8080");
 }
 
